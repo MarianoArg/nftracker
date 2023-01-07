a4b4a6632f2bade298a57e2f805c1e848e87d8e4
Guido Mariano Argañaras <arga.mariano@gmail.com>
2023-01-07 19:38:27 -0300

    Adding missing script tag

--- app/root.tsx
+++ app/root.tsx
@@ -151,6 +151,28 @@ export default function App() {
             __html: `window.ENV = ${JSON.stringify(ENV)}`,
           }}
         />
+        {process.env.NODE_ENV === "development" || !gaTrackingId ? null : (
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`}
+            />
+            <script
+              async
+              id="gtag-init"
+              dangerouslySetInnerHTML={{
+                __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${gaTrackingId}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+              }}
+            />
+          </>
+        )}
         <GlobalStateProvider>
           <ToastProvider>
             <ReservoirKitProvider
